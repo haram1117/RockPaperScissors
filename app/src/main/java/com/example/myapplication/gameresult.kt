35679532ae342cfe8b4ca85613fb68b306e961c5
Haram <haram1117@khu.ac.kr>
2021-01-11 01:32:42 +0900

    Updated User&Computer results to see

---
+++ app/src/main/java/com/example/myapplication/gameresult.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class gameresult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameresult)
+        val try_again: Button = findViewById(R.id.try_again)
+        val c_result: TextView = findViewById(R.id.computerresult)
+        val u_result:TextView = findViewById(R.id.userresult)
+
+        val result: TextView = findViewById(R.id.result)
+        val computer: Int = intent.getIntExtra("result", -1)
+        val user: Int = intent.getIntExtra("user", -1)
+        if (computer == 0)
+            c_result.setText("바위")
+        else if (computer == 1)
+            c_result.setText("보")
+        else if (computer == 2)
+            c_result.setText("가위")
+
+        if (user == 0)
+            u_result.setText("바위")
+        else if (user == 1)
+            u_result.setText("보")
+        else if (user == 2)
+            u_result.setText("가위")
+
+        if (user == computer)
+            result.setText("DRAW")
+        else if ((user == 2 && computer == 1) || (user == 1 && computer == 0) || (user == 0 && computer == 2))
+            result.setText("WIN")
+        else if ((user == 1 && computer == 2) || (user == 0 && computer == 1) || (user == 2 && computer == 0))
+            result.setText("LOSE")
+
+        try_again.setOnClickListener {
+            val intent = Intent(this@gameresult, gamestart::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/gamestart.kt
+++ app/src/main/java/com/example/myapplication/gamestart.kt
@@ -14,45 +14,26 @@ class gamestart : AppCompatActivity() {
         setContentView(R.layout.activity_gamestart)
 
         val rock: ImageView = findViewById(R.id.rock)
-        val paper:ImageView = findViewById(R.id.paper)
-        val scissors:ImageView = findViewById(R.id.scissors)
-        val result:TextView = findViewById(R.id.result)
-        val try_again:Button = findViewById(R.id.try_again)
-        val tomain:ImageView = findViewById(R.id.tomain)
+        val paper: ImageView = findViewById(R.id.paper)
+        val scissors: ImageView = findViewById(R.id.scissors)
+        val tomain: ImageView = findViewById(R.id.tomain)
         var random = Random()
         var num = random.nextInt(3) // 0: rock, 1: paper, 2: scissors
-
-        rock.setOnClickListener{
-            if(num == 0)
-                result.setText("DRAW")
-            else if(num == 1)
-                result.setText("LOSE")
-            else if(num == 2)
-                result.setText("WIN")
-        }
-        scissors.setOnClickListener{
-            if(num == 0)
-                result.setText("LOSE")
-            else if(num == 1)
-                result.setText("WIN")
-            else if(num == 2)
-                result.setText("DRAW")
-        }
-        paper.setOnClickListener{
-            if(num == 0)
-                result.setText("WIN")
-            else if(num == 1)
-                result.setText("DRAW")
-            else if(num == 2)
-                result.setText("LOSE")
-        }
-
-        try_again.setOnClickListener {
-            result.setText("RESULT")
-            val intent = Intent(this@gamestart, gamestart::class.java)
+        val intent = Intent(this@gamestart, gameresult::class.java)
+        intent.putExtra("result", num)
+        rock.setOnClickListener {
+            intent.putExtra("user", 0)
             startActivity(intent)
         }
-        tomain.setOnClickListener{
+        scissors.setOnClickListener {
+            intent.putExtra("user", 2)
+            startActivity(intent)
+        }
+        paper.setOnClickListener {
+            intent.putExtra("user", 1)
+            startActivity(intent)
+        }
+        tomain.setOnClickListener {
             val intent2 = Intent(this@gamestart, MainActivity::class.java)
             startActivity(intent2)
         }
